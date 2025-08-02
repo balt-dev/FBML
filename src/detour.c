2ed9ed86f1af10ea4077456735b4d22f4cc3b914
balt-dev <heptor42+git@gmail.com>
2025-08-02 00:05:43 -0500

    Fix gitignore

--- src/detour.c
+++ src/detour.c
@@ -6,30 +6,29 @@
 #include <stdbool.h>
 
 // Trampoline injection
-bool injectDetour(void *original, void *hook, void **trampoline) {
-  #define STOLEN_BYTES 7
+bool injectDetour(void *original, void *hook, void **trampoline, size_t stolenBytes) {
   #define JMP_SIZE 5
   #define JMP 0xE9
   #define NOP 0x90
 
   if (trampoline) {
-    *trampoline = VirtualAlloc(NULL, STOLEN_BYTES + JMP_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    *trampoline = VirtualAlloc(NULL, stolenBytes + JMP_SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     if (!*trampoline) {
         printf("Error: Failed to allocate memory for trampoline.\n");
         return false;
     }
-    memcpy(*trampoline, original, STOLEN_BYTES);
+    memcpy(*trampoline, original, stolenBytes);
 
-    LPBYTE jumpBackLocation = (LPBYTE)*trampoline + STOLEN_BYTES;
+    LPBYTE jumpBackLocation = (LPBYTE)*trampoline + stolenBytes;
     *jumpBackLocation = JMP;
 
-    LPBYTE jumpBackAddress = original + STOLEN_BYTES;
+    LPBYTE jumpBackAddress = original + stolenBytes;
     DWORD jumpOffset = (DWORD)(jumpBackAddress - (jumpBackLocation + JMP_SIZE));
     *((DWORD*)(jumpBackLocation + 1)) = jumpOffset;
   }
 
   DWORD oldProtect = 0;
-  if (!VirtualProtect(original, STOLEN_BYTES, PAGE_EXECUTE_READWRITE, &oldProtect)) {
+  if (!VirtualProtect(original, stolenBytes, PAGE_EXECUTE_READWRITE, &oldProtect)) {
       printf("Error: VirtualProtect failed to change permissions on target function.\n");
       return false;
   }
@@ -39,11 +38,11 @@ bool injectDetour(void *original, void *hook, void **trampoline) {
   DWORD hookOffset = (((LPBYTE) hook) - (originalAddr + JMP_SIZE));
   *((DWORD*)(originalAddr + 1)) = hookOffset;
 
-  for (size_t i = JMP_SIZE; i < STOLEN_BYTES; ++i) {
+  for (size_t i = JMP_SIZE; i < stolenBytes; ++i) {
       originalAddr[i] = NOP;
   }
 
-  if (!VirtualProtect(original, STOLEN_BYTES, oldProtect, &oldProtect)) {
+  if (!VirtualProtect(original, stolenBytes, oldProtect, &oldProtect)) {
       printf("Error: VirtualProtect failed to restore permissions.\n");
       return false;
   }
